b0ba6fa3ff0726602410070a003189243d7bd215
Vadzim Mikalayeu <vadzim.mikalayeu@instinctools.ru>
2018-12-10 15:41:41 +0300

    [no ticket] - updated fastlane

--- RWBelarus/Manager/RateManager.swift
+++ RWBelarus/Manager/RateManager.swift
@@ -12,14 +12,16 @@ import StoreKit
 @available(iOS 10.3, *)
 class RateManager {
     
+    private static let kRunCount = "run_count"
+    
     class func incrementCount() {
         
         // get current number of times app has been launched
-        let count = UserDefaults.standard.integer(forKey: "run_count")
+        let count = UserDefaults.standard.integer(forKey: kRunCount)
         
-        if count < 20 {
+        if count < 15 {
             // increment received number by one
-            UserDefaults.standard.set(count + 1, forKey: "run_count")
+            UserDefaults.standard.set(count + 1, forKey: kRunCount)
             // save changes to disk
             UserDefaults.standard.synchronize()
         }
@@ -27,15 +29,15 @@ class RateManager {
     
     class func showRatesController() {
         // get current number of times app has been launched
-        let count = UserDefaults.standard.integer(forKey: "run_count")
+        let count = UserDefaults.standard.integer(forKey: kRunCount)
         
-        if count == 20 {
+        if count == 15 {
             // show view with rate
             DispatchQueue.main.asyncAfter(deadline: .now() + 2, execute: {
                 SKStoreReviewController.requestReview()
             })
             // reset the number of times app has been launched
-            UserDefaults.standard.set(0, forKey: "run_count")
+            UserDefaults.standard.set(0, forKey: kRunCount)
             // save changes to disk
             UserDefaults.standard.synchronize()
         }
